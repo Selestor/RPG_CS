552cb6c021e2d51985a9fa868234c6f931f06ea6
Klaudia Augustyńska <klaudynka@mat.umk.pl>
2016-06-06 15:26:26 +0200

    kontrolki do listy postaci

--- rpg_cs_mobile/www/js/controllers/CharListController.js
+++ rpg_cs_mobile/www/js/controllers/CharListController.js
@@ -4,6 +4,28 @@
 
 mainApp.controller("CharListController", function($scope, $location){
     
+	$scope.charList = [{
+			id : 1,
+			name : 'Klaudia',
+			race : 'Elf',
+			type : 'Wojownik',
+			level : 2
+		},
+		{
+			id : 1,
+			name : 'Roman',
+			race : 'Pół elf pół wiedźmin',
+			type : 'Wojownik',
+			level : 2
+		},
+		{
+			id : 1,
+			name : 'Patryk',
+			race : 'Prawie elf',
+			type : 'Wojownik',
+			level : 2
+		},];
+	
     $scope.goToAttr = function(){
         $location.path('/charAttr');
     };
