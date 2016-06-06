957a2c66f2849ea6b0300080b2f2982b2b9bb564
Klaudia Augustyńska <klaudynka@mat.umk.pl>
2016-06-06 21:04:19 +0200

    krok 2 tworzenia postaci (atrybuty)

--- rpg_cs_mobile/www/js/controllers/NewCharAttrController.js
+++ rpg_cs_mobile/www/js/controllers/NewCharAttrController.js
@@ -5,5 +5,14 @@
 mainApp.controller("NewCharAttrController", function($scope){
     
     $scope.editEnabled = true;
+	
+	$scope.attrList = [
+		{name : 'Siła', value : 7},
+		{name : 'Zręczność', value : 15},
+		{name : 'Budowa', value : 10},
+		{name : 'Inteligencja', value : 20},
+		{name : 'Roztropność', value : 1},
+		{name : 'Charyzma', value : 3},
+	];
     
 });
