0880fe88a09d4d4b0685ecf9f2ecebea1e43f905
Klaudia Augustyńska <klaudynka@mat.umk.pl>
2016-06-07 16:38:19 +0200

    krok 6 (opisy) - ostatni krok

--- rpg_cs_mobile/www/js/controllers/NewCharInfoController.js
+++ rpg_cs_mobile/www/js/controllers/NewCharInfoController.js
@@ -3,5 +3,18 @@
  */
 
 mainApp.controller('NewCharInfoController', function($scope){
-    
+    $scope.stuffList = [];
+	$scope.addToStuff = function(name, quantity)
+	{
+		$scope.stuffList.push({name: name, quantity : quantity});
+	};
+	$scope.removeFromStuff = function(name)
+	{
+		for (var i = 0; i < $scope.stuffList.length; ++i)
+			if ($scope.stuffList[i].name == name)
+			{
+				$scope.stuffList.splice(i,1);
+				break;
+			}
+	};
 });
