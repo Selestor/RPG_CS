49ef47aaa7b9936014975e5b2278982025efbb1a
Klaudia Augustyńska <klaudynka@mat.umk.pl>
2016-06-07 12:03:20 +0200

    krok 4 tworzenia postaci (atuty)

--- rpg_cs_mobile/www/js/controllers/NewCharTrumpsController.js
+++ rpg_cs_mobile/www/js/controllers/NewCharTrumpsController.js
@@ -4,7 +4,19 @@
 
 mainApp.controller('NewCharTrumpsController', function($scope, $location){
     
-    
+    $scope.selectedClass = 'Wojownik';
+	$scope.classTrumpList = [
+		{id: 1, name : 'Atut1', description : 'Opis atutu 1'},
+		{id : 2, name : 'Atut2', description : 'Opis atutu 2'},
+		{id : 3, name : 'Atut3', description : 'Opis atutu 3'}
+	];
+	
+	$scope.additionalTrumpList = [
+		{id: 1, name : 'Dodatkowy atut1', description : 'Opis atutu 1'},
+		{id : 2, name : 'Dodatkowy atut2', description : 'Opis atutu 2'},
+		{id : 3, name : 'Dodatkowy atut3', description : 'Opis atutu 3'}
+	];
+	
     $scope.goToTrumps = function(){
         $location.path("/allTrumps");
     };
