15353fc887497a1dd9ad6d0491a5ff0048325127
Patryk Pyrka <p.pyrka@hotmail.com>
2016-06-09 15:01:42 +0200

    Ukrywanie elementów w trybie normalnym

--- rpg_cs_mobile/www/js/controllers/NewCharController.js
+++ rpg_cs_mobile/www/js/controllers/NewCharController.js
@@ -2,7 +2,26 @@
  * Created by pyrkap on 28.05.2016.
  */
 
-mainApp.controller("NewCharController", function($scope){
+mainApp.controller("NewCharController", function($rootScope, $scope){
+
+	/**
+	 *
+	 * Flaga normal_mode - gwarantująca przechowanie informacji czy użytkownik wybrał tryb normalny czy domowy.
+	 * True, jeśli normalny, false wpp. Flaga dostępna jest z poziomu każdego widoku/kontrolera przez umieszczenie jej
+	 * w rootScope.
+	 *
+	 */
+	if($rootScope.normal_mode === undefined)
+		$rootScope.normal_mode = true;
+
+	/**
+	 * Zmienia wartość flagi normal_mode.
+	 * Funkcja dołączona do rootScope, gwarantuje dostępność z poziomu każdego widoku.
+	 * @param flag - nowa flaga (true|false)
+     */
+	$rootScope.setNormalMode = function(flag){
+		$rootScope.normal_mode = flag;
+	};
 
 	$scope.race = {};
 	 // $scope.race.selected = $scope.raceList[3];
