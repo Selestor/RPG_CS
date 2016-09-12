378a1e8fc2862bc3ffaf7e359a8d318013466af4
Admin <Admin>
2016-09-12 17:28:32 +0200

    umiejetnosci, atuty i KP

--- rpg_cs_mobile/www/js/controllers/CharAbiAndTrumpsController.js
+++ rpg_cs_mobile/www/js/controllers/CharAbiAndTrumpsController.js
@@ -11,4 +11,32 @@ mainApp.controller('CharAbiAndTrumpsController', function($rootScope, $scope, $l
     };
 	
 	$scope.current = $rootScope.currentChar;
+	
+	$scope.ArmorClass = calculateAC(calculateModifier($scope.current.attrList[1]), $scope.current.selectedArmors[0].power,  $scope.current.selectedArmors[0].type)
 });
+
+function calculateModifier(attributeValue) {
+	return ((attributeValue - 10)/2) ;
+}
+
+function calculateAC(dexAC, armorAC, armorType) 
+	if (armorAC === undefined || armorAC == null)
+	{
+		armorAC = 10;
+		maxDexAC = 20;
+	}
+	switch(armorType)
+	{
+		case "Lekka":
+			dexAC = dexAC
+			break;
+		case "Średnia":
+			if (dexAC > 2) dexAC = 2
+			else dexAC = dexAC
+			break;
+		case "Ćiężka":
+			dexAC = 0
+			break;
+	}
+	return (dexAC + armorAC);
+}
